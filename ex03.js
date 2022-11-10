6ad3de0867c7cd47b12ff22a55a8abdf452ce00e
André Casal <andre@andrecasal.com>
2022-11-10 17:24:59 +0000

    Initial commit

---
+++ ex03.js
@@ -0,0 +1,7 @@
+/* You are given a string of space separated numbers, and have to return the highest and lowest number */
+
+
+
+console.log(highAndLow("1 2 3 4 5"))  // "5 1"
+console.log(highAndLow("1 2 -3 4 5")) // "5 -3"
+console.log(highAndLow("1 9 3 4 -5")) // "9 -5"
---
+++ ex04.js
@@ -0,0 +1,12 @@
+/* Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels (but not y).
+
+The input string will only consist of lower case letters and/or spaces. */
+
+
+
+console.log(getCount(`roses are red`)) // 5
+console.log(getCount(`violets are blue`)) // 7
+console.log(getCount(`i can't rhyme`)) // 3
+console.log(getCount(`banana`)) // 3
